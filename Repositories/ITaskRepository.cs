b428077d255ba8361315930d879b5d18affebae8
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-11-15 17:17:14 -0300

    feat: coloca os metodos de update e delete na camada de repository

--- Repositories/ITaskRepository.cs
+++ Repositories/ITaskRepository.cs
@@ -7,7 +7,7 @@ public interface ITaskRepository
 {
     public Task<List<Tasks>> GetAllTasks();
     public Task<Tasks> GetTaskById(int id);
-    public void AddTask();
-    public void UpdateTask();
-    public void DeleteTask();
+    public Task AddTask(Tasks task);
+    public Task UpdateTask(Tasks task);
+    public Task DeleteTask(Tasks task);
 }
--- Repositories/TaskRepository.cs
+++ Repositories/TaskRepository.cs
@@ -16,7 +16,7 @@ public class TaskRepository : ITaskRepository
     }
 
 
-    public async Task <List<Tasks>> GetAllTasks()
+    public async Task<List<Tasks>> GetAllTasks()
     {
         return await _context.Tasks.ToListAsync();
     }
@@ -32,19 +32,35 @@ public class TaskRepository : ITaskRepository
     }
 
 
-    public void AddTask()
+    public async Task AddTask(Tasks task)
     {
-
+        _context.Tasks.Add(task);
+        await _context.SaveChangesAsync();
     }
 
-    public void UpdateTask()
+    public async Task UpdateTask(Tasks task)
     {
 
+        var existingTask = await _context.Tasks.FindAsync(task.Id);
+
+        if (existingTask is null)
+            throw new InvalidOperationException($"Task with id {task.Id} not found.");
+
+        existingTask.Title = task.Title;
+        existingTask.Description = task.Description;
+        existingTask.UpdatedAt = DateTime.UtcNow;
+
+        _context.Tasks.Update(task);
+        await _context.SaveChangesAsync();
     }
-    
-    public void DeleteTask()
+
+    public Task DeleteTask(Tasks task)
     {
-        
+        _context.Tasks.Remove(task);
+        return _context.SaveChangesAsync();
     }
 
 }
+
+
+//todo: entender o metodo update e se precisa instalar algum Mapper, abrir PR no github e criar camada de servico (service) entre controller e repository, criar camada de controller, 
