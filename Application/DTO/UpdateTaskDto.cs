661a691e567114bb33e1c37494da282ebf6f9e08
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-12-12 17:08:34 -0300

    feat: adiciona validação para os campos de CreateTaskDTO e UpdateTaskDTO

--- Application/DTO/UpdateTaskDto.cs
+++ Application/DTO/UpdateTaskDto.cs
@@ -1,9 +1,19 @@
-﻿namespace CRUD_ASPNET.Application.DTO
+﻿using System.ComponentModel.DataAnnotations;
+using CRUD_ASPNET.Models;
+
+namespace CRUD_ASPNET.Application.DTO
 {
     public class UpdateTaskDTO
     {
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 200 characters")]
         public string? Title { get; set; }
+
+        [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters")]
         public string? Description { get; set; }
-        public CRUD_ASPNET.Models.TaskStatus? Status { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        [EnumDataType(typeof(Models.TaskStatus), ErrorMessage = "Status must be ToDo (1), Doing (2), or Done (3)")]
+        public Models.TaskStatus? Status { get; set; }
     }
 }
