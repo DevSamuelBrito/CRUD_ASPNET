083081cc11ed902891d18c40a077bdd8ddc60caa
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-12-12 16:49:25 -0300

    feat:  arruma o program e instala as dependencias necessarios para o banco

--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
+using CRUD_ASPNET.Configuration.Context;
 using CRUD_ASPNET.Repositories;
 using CRUD_ASPNET.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
+//swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//auto mapper
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//db context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+//Injection of dependencies
 builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<ITaskService, TaskService>();
+
+//CORS
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
 var swaggerSettings = builder.Configuration.GetSection("Swagger");
 var routePrefix = swaggerSettings.GetValue<string>("RoutePrefix") ?? "api/docs";
 var jsonRouteTemplate = swaggerSettings.GetValue<string>("JsonRouteTemplate");
-
-
 
 if (app.Environment.IsDevelopment())
 {
@@ -51,4 +75,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors();
+
+app.MapControllers();
+
 app.Run();
+
+// dev: http://localhost:5272/api/docs/index.html
